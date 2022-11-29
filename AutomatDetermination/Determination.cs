c6b8f05ebad1361a069710d21187f05ee471144b
PolkanDot <insarmiftahov@yandex.ru>
2022-11-29 21:09:23 +0300

    Added AutomatDetermination project. Added ability to read input automat

---
+++ AutomatDetermination/Determination.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApplication
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            // Подкотовка к чтению таблицы
+            string[] mas = Console.ReadLine().Split();
+            int k = Convert.ToInt32(mas[0]);
+            int m = Convert.ToInt32(mas[1]);
+            string[,] initialStates = new string[k, m + 1];
+
+            //Все вспомогательные переменные
+            int line, column = 0;
+
+            // Вспомогательная таблица для хранения eclose каждого начального состояния
+            List<List<string>> ecloses = new List<List<string>>();
+
+            // Заполнение первичной таблицы входными данными и присваивание первых групп
+            for (line = 0; line < k; line++)
+            {
+                mas = Console.ReadLine().Split(" ");
+                for (column = 0; column < m + 1; column++)
+                {
+                    initialStates[line, column] = mas[column];
+                }         
+            }
+        }
+    }
+}
