a7e88c7af60dae082540b77a9ba9c607760090f3
PolkanDot <insarmiftahov@yandex.ru>
2022-11-30 21:28:54 +0300

    Added first step to creating result table

--- AutomatDetermination/Determination.cs
+++ AutomatDetermination/Determination.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApplication
+﻿using System.Linq;
+
+namespace ConsoleApplication
 {
     class MainProgram
     {
@@ -29,10 +31,16 @@
             //Все вспомогательные переменные
             int line, column = 0;
 
-            // Вспомогательная таблица для хранения eclose каждого начального состояния
+            // Вспомогательный список для хранения eclose каждого начального состояния
             List<List<string>> ecloses = new List<List<string>>();
 
-            // Заполнение первичной таблицы входными данными и присваивание первых групп
+            // Вспомогательный список для определения завершения детерминирования
+            List<List<string>> remainingStates = new List<List<string>>();
+
+            // Таблица результата детерминирования
+            List<List<string>> resultAutomat = new List<List<string>>();
+
+            // Заполнение первичной таблицы входными данными
             for (line = 0; line < k; line++)
             {
                 mas = Console.ReadLine().Split(" ");
@@ -41,9 +49,7 @@
                     initialStates[line, column] = mas[column];
                 }         
             }
-
-            
-
+            // Поиск eclose-ов для каждого состояния
             for (column = 0; column < k; column++)
             {
                 List<string> workList = new List<string>();
@@ -51,12 +57,44 @@
                 SearchEclose(ref initialStates, ref workList, column, m);
                 ecloses.Add(workList);
             }
+
+            List<string> startList = new List<string>();
+            startList.Add("0");
+            remainingStates.Add(startList);
+            startList = new List<string>();
+            startList.Add("1");
+            startList.Add("2");
+            remainingStates.Add(startList);
+            startList = new List<string>();
+            startList.Add("1");
+            startList.Add("2");
+            startList.Add("3");
+            remainingStates.Add(startList);
+            startList = new List<string>();
+            startList.Add("4");
+            remainingStates.Add(startList);
+            List<string> currentEcloseList = new List<string>();
+            string currentEcloseString;
+            string[] allSymbols;
+            HashSet<string> uniqueSymbolsSet = new HashSet<string>();
             Console.WriteLine();
-            for (line = 0; line < ecloses.Count; line++)
+
+            while (remainingStates.Count > 0)
             {
-                for (column = 0; column < ecloses[line].Count; column++)
+                currentEcloseList = remainingStates[0];
+                currentEcloseString = "";
+                for (column = 0; column < currentEcloseList.Count; column++)
                 {
-                    Console.Write(ecloses[line][column] + " ");
+                    currentEcloseString += "," + (string.Join(",", ecloses[Convert.ToInt32(currentEcloseList[column])]));
+                }
+                allSymbols = currentEcloseString.Split(",");
+                uniqueSymbolsSet = new HashSet<string>(allSymbols);
+                currentEcloseList = uniqueSymbolsSet.ToList();
+                currentEcloseList.RemoveAt(0);
+                remainingStates.RemoveAt(0);
+                for (column = 0; column < currentEcloseList.Count; column++)
+                {
+                    Console.Write(currentEcloseList[column] + " ");
                 }
                 Console.WriteLine();
             }
